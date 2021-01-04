b0ac6b2a02f1f9d268038531b8b727451447fb15
haoxin <haoxinst@gmail.com>
2021-01-04 20:28:15 +0800

    fmt
    Azure Cosmos DB

--- build.py
+++ build.py
@@ -8,29 +8,25 @@ contents = []
 
 dir = os.path.join(rootdir, 'articles')
 for (dirpath, dirname, filenames) in os.walk(dir):
-    for filename in filenames:
-        p = os.path.join(dir, filename)
-        info = os.stat(p)
-        file_infos.append({
-            'mtime': info.st_mtime,
-            'path': p,
-            'name': filename
-        })
+  for filename in filenames:
+    p = os.path.join(dir, filename)
+    info = os.stat(p)
+    file_infos.append({'mtime': info.st_mtime, 'path': p, 'name': filename})
 
 
 def takeMtime(info):
-    return info["mtime"]
+  return info["mtime"]
 
 
 file_infos.sort(key=takeMtime, reverse=True)
 
 for i in file_infos:
-    file = open(i['path'], 'r')
-    lines = file.readlines()
-    title = lines[1].split(':').pop().strip()
-    content = '* [' + title + '](articles/' + i['name'] + ')'
-    contents.append(content)
-    file.close()
+  file = open(i['path'], 'r')
+  lines = file.readlines()
+  title = lines[1].split(':').pop().strip()
+  content = '* [' + title + '](articles/' + i['name'] + ')'
+  contents.append(content)
+  file.close()
 
 file = open(os.path.join(rootdir, 'index.md'), 'w')
 file.write('\n'.join(contents))
