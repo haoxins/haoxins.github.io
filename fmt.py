22c08dbb770f0bad0e6e7f48d060efa5668dc361
haoxin <haoxinst@gmail.com>
2021-07-12 21:16:24 +0800

    use py

--- fmt.py
+++ fmt.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
+import subprocess
+from os import path
+
+root_dir = path.dirname(path.realpath(__file__))
 
 
-fmt_md() {
-  MD_FILE="./$MD_FILE"
+def fmt_md(file: str):
+    file = path.join(root_dir, file)
+    with open(file, "r+") as f:
+        text = f.read()
+        text = (
+            text.replace("\t", " ", -1)
+            .replace("’", "'", -1)
+            .replace("‘", "'", -1)
+            .replace("“", '"', -1)
+            .replace("”", '"', -1)
+            .replace("（", " (", -1)
+            .replace("）", ") ", -1)
+            .replace("【", " [", -1)
+            .replace("】", "] ", -1)
+            .replace("《", " <", -1)
+            .replace("》", "> ", -1)
+            .replace("，", ", ", -1)
+            .replace("、", ", ", -1)
+            .replace("。", ". ", -1)
+            .replace("；", "; ", -1)
+            .replace("：", ": ", -1)
+            .replace("！", "! ", -1)
+            .replace("？", "? ", -1)
+        )
 
-  if [ -f $MD_FILE ]; then
-    cat $MD_FILE | \
-    tr '\t' ' ' | \
-    tr '’' "'" | \
-    tr '‘' "'" | \
-    tr '“' '"' | \
-    tr '”' '"' | \
-    tr '（' '(' | \
-    tr '）' ')' | \
-    tr '【' '[' | \
-    tr '】' ']' | \
-    tr '《' '<' | \
-    tr '》' '>' | \
-    tr '，' ', ' | \
-    tr '、' ', ' | \
-    tr '。' '. ' | \
-    tr '；' '; ' | \
-    tr '：' ': ' | \
-    tr '！' '! ' | \
-    tr '？' '? ' > \
-    $MD_FILE
-  else
-    echo "$MD_FILE does not exist."
-  fi
-}
+        f.seek(0)
+        f.write(text)
+        f.truncate()
 
-MD_FILES=`git add --all && git status -s | grep '.md' | grep -E 'A|M' | cut -c 2- | xargs`
 
-for MD_FILE in $MD_FILES
-do
-  fmt_md
-done
+subprocess.run(["git", "add", "--all"])
+output = subprocess.run(["git", "status", "-s"], capture_output=True)
+
+files = output.stdout.decode("utf-8").split("\n")
+files = filter(lambda s: s.endswith(".md"), files)
+files = map(lambda s: s[1:].strip(), files)
+
+for file in files:
+    fmt_md(file)
