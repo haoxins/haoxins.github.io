5333596d574ebef2c3759c623681961c88aa08bc
haoxin <haoxinst@gmail.com>
2021-07-12 20:36:37 +0800

    fmt

--- index.py
+++ index.py
@@ -10,32 +10,32 @@ contents = []
 
 
 def gen_contents(sub_path, reverse):
-  article_dir = path.join(root_dir, sub_path)
-  for (_, _, filenames) in os.walk(article_dir):
-    for filename in filenames:
-      p = path.join(article_dir, filename)
-      info = os.stat(p)
-      file_infos.append({'mtime': info.st_mtime, 'path': p, 'name': filename})
+    article_dir = path.join(root_dir, sub_path)
+    for (_, _, filenames) in os.walk(article_dir):
+        for filename in filenames:
+            p = path.join(article_dir, filename)
+            info = os.stat(p)
+            file_infos.append({"mtime": info.st_mtime, "path": p, "name": filename})
 
-  file_infos.sort(key=lambda info: info["name"], reverse=reverse)
+    file_infos.sort(key=lambda info: info["name"], reverse=reverse)
 
-  for i in file_infos:
-    with open(i['path'], 'r') as f:
-      lines = f.readlines()
-      title = lines[1].split(':').pop().strip()
-      content = f'* [{title}]({sub_path}/{i["name"]})'
-      contents.append(content)
+    for i in file_infos:
+        with open(i["path"], "r") as f:
+            lines = f.readlines()
+            title = lines[1].split(":").pop().strip()
+            content = f'* [{title}]({sub_path}/{i["name"]})'
+            contents.append(content)
 
-  file_infos.clear()
+    file_infos.clear()
 
 
 # Generate the index page
 
-gen_contents('articles', False)
+gen_contents("articles", False)
 
-contents.append('\n### Archived\n')
+contents.append("\n### Archived\n")
 
-gen_contents('archived', True)
+gen_contents("archived", True)
 
-with open(path.join(root_dir, 'index.md'), 'w') as f:
-  f.write('\n'.join(contents))
+with open(path.join(root_dir, "index.md"), "w") as f:
+    f.write("\n".join(contents))
