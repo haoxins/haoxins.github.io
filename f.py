280f42375d5534f7456d0c3d9d024f5968afbfde
haoxin <haoxinst@gmail.com>
2023-01-07 21:41:52 +0800

    2023

--- f.py
+++ f.py
@@ -84,13 +84,8 @@ def gen_contents(sub_path):
 
 
 def gen_index():
-    gen_contents("articles")
-    index_contents.append("### 2024")
-    gen_contents("archived-2024")
     index_contents.append("### 2023")
     gen_contents("archived-2023")
-    index_contents.append("### 2022")
-    gen_contents("archived-2022")
 
     with open(path.join(root_dir, "index.md"), "w") as f:
         f.write("\n".join(index_contents))
